028ebcd2a08380dd8938cee96266f738aee1da63
Eli Lewis <elilewis327@gmail.com>
2025-03-21 22:21:33 -0400

    asdf

--- trash_db.sql
+++ trash_db.sql
@@ -1,8 +1,10 @@
 DROP DATABASE IF EXISTS trash_db;
-CREATE DATABASE trash_db OWNER PGAdmin;
+CREATE DATABASE trash_db;
 
-DROP TABLE cans IF EXISTS;
-CREATE TABLE cans OWNER PGAdmin (
+CREATE SCHEMA trash;
+
+DROP TABLE trash.cans IF EXISTS;
+CREATE TABLE trash.cans(
     id SERIAL PRIMARY KEY,
     loc_x INT,
     loc_y INT,
@@ -12,8 +14,8 @@ CREATE TABLE cans OWNER PGAdmin (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-DROP TABLE trash_data IF EXISTS;
-CREATE TABLE trash_data OWNER PGAdmin (
+DROP TABLE trash.data IF EXISTS;
+CREATE TABLE trash.data(
     sensor_id INT,
     transaction_id SERIAL,
     PRIMARY KEY (sensor_id, transaction_id),
@@ -24,5 +26,5 @@ CREATE TABLE trash_data OWNER PGAdmin (
     battery_level INT,
     signal_strength INT,
 
-    FOREIGN KEY (sensor_id) REFERENCES cans(id) ON DELETE CASCADE
+    FOREIGN KEY (sensor_id) REFERENCES trash.cans(id) ON DELETE CASCADE
     );
