b0cc1ea7fa261e3e8c2b5952fdf7ac8b3f44815e
Eli Lewis <elilewis327@gmail.com>
2025-03-22 00:30:42 -0400

    squeel changes

--- trash_db.sql
+++ trash_db.sql
@@ -1,9 +1,7 @@
-DROP DATABASE IF EXISTS trash_db;
 CREATE DATABASE trash_db;
 
 CREATE SCHEMA trash;
 
-DROP TABLE trash.cans IF EXISTS;
 CREATE TABLE trash.cans(
     id SERIAL PRIMARY KEY,
     loc_x INT,
@@ -14,13 +12,12 @@ CREATE TABLE trash.cans(
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-DROP TABLE trash.data IF EXISTS;
 CREATE TABLE trash.data(
     sensor_id INT,
     transaction_id SERIAL,
     PRIMARY KEY (sensor_id, transaction_id),
     
-    created_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
     value INT,
     depth INT,
     battery_level INT,
@@ -28,3 +25,25 @@ CREATE TABLE trash.data(
 
     FOREIGN KEY (sensor_id) REFERENCES trash.cans(id) ON DELETE CASCADE
     );
+    
+INSERT INTO trash.cans (loc_x, loc_y, floor_number, building, room_number) 
+SELECT 
+    (random() * 200)::INT AS loc_x, 
+    (random() * 200)::INT AS loc_y, 
+    (floor(random() * 5) + 1) AS floor_number, 
+    (ARRAY['SB', 'HH', 'NH', 'HL', 'SC'])[floor(random() * 5) + 1] AS building, 
+    (floor(random() * 300) + 100)::TEXT AS room_number
+FROM generate_series(1, 100);
+
+INSERT INTO trash.data (sensor_id, value, depth, battery_level, signal_strength) 
+SELECT
+	((random() * 199)::INT+1) AS sensor_id,
+	(random() * 100)::INT AS value,
+	(random() * 100)::INT AS depth,
+	(random() * 100)::INT AS battery_level,
+	(random() * 100)::INT AS signal_strength
+FROM generate_series(1,100);
+
+
+select * from trash.cans;
+select * from trash.data;
